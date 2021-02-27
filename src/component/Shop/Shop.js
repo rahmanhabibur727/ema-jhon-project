53f2a7191b78a9d21aade4fc6b976e8cef403360
unknown <rahmanhabibur727@gemail.com>
2021-02-27 13:24:58 +0600

    project is running

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+
+const Shop = () => {
+    const first15 = fakeData.slice(0, 15);
+    const [products, setProducts] = useState(first15);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) =>{
+        console.log('product added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    products.map(pd => <Product
+                        handleProduct = {handleAddProduct}
+                         product={pd}></Product>)
+                }  
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
